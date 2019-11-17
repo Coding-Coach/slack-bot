7791093f94790de8b9136b3f4e4c1d1b2f3c3aa3
Mohit Tater <mohittater@bluejeans.com>
2019-11-17 19:21:45 +0530

    modified:   app.js 	new file:   messages.json 	modified:   package.json 	modified:   yarn.lock
    	modified:   app.js
    	new file:   messages.json
    	modified:   package.json
    	modified:   yarn.lock

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
-const express = require('express')
-const app = express()
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
+const {App} = require("@slack/bolt")
+const welcomeMsg = require('./messages.json')
+
+const BOT_ICON_URL = `https://raw.githubusercontent.com/Coding-Coach/find-a-mentor/master/public/codingcoach-logo-512.png`
+const SLACK_BOT_TOKEN =  process.env.SLACK_BOT_TOKEN
+const SLACK_SIGNING_SECRET  =  process.env.SLACK_SIGNING_SECRET
+
+const app = new App({
+  token: SLACK_BOT_TOKEN,
+  signingSecret: SLACK_SIGNING_SECRET
+})
+
+const formatMsgForUser = (user) => {
+  return JSON.stringify(welcomeMsg).replace('%user-name%', user)
 }
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// As of now, there is no requirement for this event, but we can use it for quick
+// testing as team_join event is not easy to test
+app.event("app_mention", async ({ event, context }) => {
+  console.log(event)
+  const welcomeMsg = formatMsgForUser(event.user)
+  try {
+    const result = await app.client.chat.postMessage({
+      token: context.botToken,
+      channel: event.user,
+      as_user: true,
+      link_names: true,
+      icon_url:BOT_ICON_URL,
+      blocks: welcomeMsg
+    })
+  } catch (error) {
+    console.error(error)
+  }
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.event("team_join", async ({ event, context }) => {
+  console.log(event);
+  const welcomeMsg = formatMsgForUser(event.user.id);
+  try {
+    const result = await app.client.chat.postMessage({
+      token: context.botToken,
+      channel: event.user.id,
+      as_user: true,
+      link_names: true,
+      blocks: welcomeMsg
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.start(process.env.PORT || 8000);
+console.log("CC bot is active!");
